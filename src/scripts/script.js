c576ad01d0bdcb72e143a15bd3acb1e24c515315
KACHIG <jose64rosado@gmail.com>
2024-12-04 19:18:53 -0500

    Made changes to HTML and made separate CSS file
    Made changes to HTML like adding the gameInfo DIV and making CSS its own file also made changes to some functions in the Javascript to make the randomGame function work better

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,7 @@
-apiKey = "rJ0HxvXQ6t4H1e2MXzzpXlhFmRfAyO83QAearWi1"
+apiKey = "rJ0HxvXQ6t4H1e2MXzzpXlhFmRfAyO83QAearWi1";
+
+let uniqueNumbers = [5];
+let findGame = 0;
 
 async function getCount() {
     const myHeaders = new Headers();
@@ -17,9 +20,8 @@ async function getCount() {
     let response = await fetch("https://ho8o8ytc66.execute-api.us-west-2.amazonaws.com/production/v4/games/count", requestOptions)
   
     let data = await response.json();
-    console.log(data.count)
     return data.count;
-  }
+  };
 
 async function getCover(coverId) {
     const myHeaders = new Headers();
@@ -40,13 +42,16 @@ async function getCover(coverId) {
     let data = await response.json();
     let imageId = data[0].image_id
     return 'https://images.igdb.com/igdb/image/upload/t_cover_big/' + imageId +'.jpg'
-}
+};
   
 async function getRandomGame() {
     let gameCount = await getCount();
-    let findGame = Math.floor(Math.random() * gameCount) + 1;
-    console.log(findGame);
-    
+    findGame = Math.floor(Math.random() * gameCount) + 1;
+    while (uniqueNumbers.includes(findGame)) {
+      findGame = Math.floor(Math.random() * gameCount) + 1; // Generate random number between 1 and 100
+    }
+    uniqueNumbers.push(findGame);
+
     const myHeaders = new Headers();
     myHeaders.append("x-api-key", apiKey);
     myHeaders.append("Content-Type", "application/javascript");
@@ -66,11 +71,44 @@ async function getRandomGame() {
     
     let cover = await getCover(data[0].cover);
     return cover
-  }
+  };
 
-let test = getRandomGame();
-console.log(test)
+async function gameSearch() {
+  
+
+  const myHeaders = new Headers();
+  myHeaders.append("x-api-key", apiKey);
+  myHeaders.append("Content-Type", "application/javascript");
+
+  const raw = "search \"Halo\"; fields name;";
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  fetch("https://ho8o8ytc66.execute-api.us-west-2.amazonaws.com/production/v4/games", requestOptions)
+    
+};
 
 function displayCarousel() {
+  getRandomGame().then(value => {
+    document.getElementById("ci1").src = value
+  });
+  getRandomGame().then(value => {
+    document.getElementById("ci2").src = value
+  });
+  getRandomGame().then(value => {
+    document.getElementById("ci3").src = value
+  });
+  getRandomGame().then(value => {
+    document.getElementById("ci4").src = value
+  });
+  getRandomGame().then(value => {
+    document.getElementById("ci5").src = value
+  });
+};
 
-}
+displayCarousel();
